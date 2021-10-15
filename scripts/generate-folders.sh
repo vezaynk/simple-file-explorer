38ea7f43f8be339f027f9215e06218979e547c6b
Slava Knyazev <slava@knyazev.io>
2021-10-15 16:58:09 -0400

    use local paths for generated files

--- scripts/generate-folders.sh
+++ scripts/generate-folders.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm -rf /tmp/folders/
-mkdir -p /tmp/folders/{root1/deeply/nested/folder/structure,root2/{folder1,folder2,folder3},root3}
+rm -rf tmp/folders/
+mkdir -p tmp/folders/{root1/deeply/nested/folder/structure,root2/{folder1,folder2,folder3},root3}
 
-touch /tmp/folders/root1/deeply/nested/folder/structure/the_end.docx
-touch /tmp/folders/root2/diary.txt
-touch /tmp/folders/root2/{folder1,folder2,folder3}/nested_files.txt
+touch tmp/folders/root1/deeply/nested/folder/structure/the_end.docx
+touch tmp/folders/root2/diary.txt
+touch tmp/folders/root2/{folder1,folder2,folder3}/nested_files.txt
 
-touch /tmp/folders/root3/{file1.js,file2.php,another_file.gif,zzzzz.png}
+touch tmp/folders/root3/{file1.js,file2.php,another_file.gif,zzzzz.png}
