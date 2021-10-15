27dbf97a9f303cd5a11840b50d68a7afe6f2ac1f
Slava Knyazev <slava@knyazev.io>
2021-10-15 03:47:00 -0400

    remove ts-ignore

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,7 @@ import http from "http";
 import { registerWebSocketServer } from "./WSService";
 import serve from "koa-static";
 import path from "path";
+import { AddressInfo } from "net";
 
 const app = new Koa();
 
@@ -12,6 +13,5 @@ registerWebSocketServer(server);
 app.use(serve(path.join(__dirname, "..", "public")));
 
 server.listen(8080, function listening() {
-  // @ts-ignore
-  console.log("Listening on", server.address().port);
+  console.log("Listening on", (server.address() as AddressInfo).port);
 });
