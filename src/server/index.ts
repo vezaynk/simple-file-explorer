8ecb16c491835aed3b4445df00dde4066d998fbb
Slava Knyazev <slava@knyazev.io>
2021-10-15 17:08:10 -0400

    startup error handling

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,24 +7,32 @@ import { AddressInfo } from "net";
 import { getFileType } from "./FileWatcher";
 import open from "open";
 
-const app = new Koa();
+async function main(args: string[]) {
+  const app = new Koa();
 
-const server = http.createServer(app.callback());
+  const server = http.createServer(app.callback());
 
-const roots = process.argv.slice(2).map((p) => path.resolve(p));
-for (let root of roots) {
-  getFileType(root).then((type) => {
-    if (type != "folder") {
-      throw new Error(`${root} is not a folder.`);
+  const roots = args.map((p) => path.resolve(p));
+  try {
+    for (let root of roots) {
+      const type = await getFileType(root);
+      if (type != "folder") {
+        throw new Error(`${root} is not a folder.`);
+      }
     }
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+
+  registerWebSocketServer(server, roots);
+  app.use(serve(path.join(__dirname, "..", "public")));
+
+  server.listen(process.env.PORT, function listening() {
+    const port = (server.address() as AddressInfo).port;
+    console.log("Listening on", port);
+    if (process.env.OPEN) open("http://localhost:" + port);
   });
 }
 
-registerWebSocketServer(server, roots);
-app.use(serve(path.join(__dirname, "..", "public")));
-
-server.listen(process.env.PORT, function listening() {
-  const port = (server.address() as AddressInfo).port;
-  console.log("Listening on", port);
-  if (process.env.OPEN) open("http://localhost:" + port);
-});
+main(process.argv.slice(2));
